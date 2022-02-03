c94bbde98a3bf997f3875627e9c26c95f3dc9ca6
rlatp1409 <stanley710@naver.com>
2022-02-04 00:28:12 +0900

    initial commit Spring Starter 초기 설정

---
+++ src/main/java/com/example/CircleSearch/CircleSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.CircleSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleSearchApplication.class, args);
+	}
+
+}
